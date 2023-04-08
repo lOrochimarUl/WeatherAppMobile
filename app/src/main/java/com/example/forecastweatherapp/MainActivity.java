bd0530853f7f7ba5a3079d54757c9b6487997666
Byk Alexandr <bik.san1357911@gmail.com>
2023-04-08 14:24:48 +0300

    Add interface on the main page to test getting a forecast

--- app/src/main/java/com/example/forecastweatherapp/MainActivity.java
+++ app/src/main/java/com/example/forecastweatherapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.forecastweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button get_owm_forecast;
+    private TextView view_forecast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        get_owm_forecast = findViewById(R.id.get_owm_forecast);
+        view_forecast = findViewById(R.id.view_forecast);
+
+        get_owm_forecast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
