ff40c00a252bf85244161ed09f994f412f2a1127
Byk Alexandr <bik.san1357911@gmail.com>
2023-04-09 16:18:23 +0300

    Add HTTP library

--- app/src/main/java/com/example/forecastweatherapp/MainActivity.java
+++ app/src/main/java/com/example/forecastweatherapp/MainActivity.java
@@ -7,14 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import com.loopj.android.http.*;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button get_owm_forecast;
     private TextView view_forecast;
+    private String APIkey_OPW = new String();
+    private File file;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         get_owm_forecast = findViewById(R.id.get_owm_forecast);
         view_forecast = findViewById(R.id.view_forecast);
@@ -22,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         get_owm_forecast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
             }
         });
 
