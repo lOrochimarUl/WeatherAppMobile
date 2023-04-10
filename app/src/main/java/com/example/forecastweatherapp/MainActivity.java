d48c2518be0fb5672626431a96c9885d68463f00
Byk Alexandr <bik.san1357911@gmail.com>
2023-04-10 11:34:01 +0300

    Add in the project APIkey-file and put hin into the .gitignore

--- app/src/main/java/com/example/forecastweatherapp/MainActivity.java
+++ app/src/main/java/com/example/forecastweatherapp/MainActivity.java
@@ -8,7 +8,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.File;
-import com.loopj.android.http.*;
+import java.io.FileReader;
+import java.io.IOException;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +22,65 @@ public class MainActivity extends AppCompatActivity {
     private File file;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        file = new File("C:\\Users\\User\\AndroidStudioProjects\\ForecastWeatherApp\\app\\src\\main\\res\\APIkey.txt");
+        BufferedReader r = null;
+        try {
+            r = new BufferedReader(new FileReader(file.getAbsolutePath()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            String scores = r.readLine();
+            APIkey_OPW = r.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        //file = new File("C:\\Users\\User\\AndroidStudioProjects\\ForecastWeatherApp\\app\\src\\main\\res\\APIkey.txt");
+
+
+        try (FileReader reader = new FileReader(new File(getFilesDir(), "com/example/forecastweatherapp/APIkey.txt")))
+        {
+            char[] chars = new char[(int) file.length()];
+            reader.read(chars);
+
+            APIkey_OPW = new String(chars);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+      */
+
+        try(FileReader reader = new FileReader("APIkey.txt"))
+        {
+            int c;
+            while((c=reader.read())!=-1)
+            {
+                System.out.print((char)c);
+            }
+        }
+        catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        System.out.println(APIkey_OPW);
+
+
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request
+                .Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q=Budënnovsk&lang=ru&units=metric&appid=" + APIkey_OPW)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         get_owm_forecast = findViewById(R.id.get_owm_forecast);
